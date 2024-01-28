db6dc3655bc7cb897ed95555eb3c79a6ebfdcc1f
Akash saini <sainiakash1210@gmail.com>
2024-01-28 19:12:05 +0530

    create folder for both normal react and vitejs react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>First Project with React js</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+     <h1>My First Project with Vitejs | Akash</h1>
+  )
+}
+
+export default App
